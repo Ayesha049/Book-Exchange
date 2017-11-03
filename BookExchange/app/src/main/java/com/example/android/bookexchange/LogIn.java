79c1dff51fab602f30facf9ada52d9a5eb13cd34
Ayesha049 <1405049as@gmail.com>
2017-11-03 23:52:35 +0600

    adding java class and xml to my project part 1

---
+++ BookExchange/app/src/main/java/com/example/android/bookexchange/LogIn.java
@@ -0,0 +1,17 @@
+package com.example.android.bookexchange;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by l on 11/3/17.
+ */
+
+public class LogIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+    }
+}
---
+++ BookExchange1/app/src/main/java/com/example/android/bookexchange1/BuySell.java
@@ -0,0 +1,17 @@
+package com.example.android.bookexchange1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by l on 11/3/17.
+ */
+
+public class BuySell extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.buy_sell);
+    }
+}
