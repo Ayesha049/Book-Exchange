c87edd9ea971aba209227d3cbc7229545f38316c
Ayesha049 <1405049as@gmail.com>
2017-11-12 16:49:42 +0600

    updating layouts using string.xml,dimens.xml

--- app/src/main/java/com/example/android/bookexchange1/MainActivity.java
+++ app/src/main/java/com/example/android/bookexchange1/MainActivity.java
@@ -10,15 +10,28 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     // TODO: add Facebook Stetho debugging tool [re: http://facebook.github.io/stetho/]
-    //// TODO: add the source of the BOI.jpeg image to README file
-    // TODO: add UI screenshots to the README file
-    // TODO: add vector drawable assets (e.g., XML / SVG format) for all the icons identified in your mockup to drawable folder
+    // TODO: add vector drawable assets (e.g., XML / SVG format)
+    // for all the icons identified in your mockup to drawable folder
     // TODO: implement vector drawables in UI according to wireframes
-    // TODO: add hard-coded strings to string.xml; consider using xliff:g and / or Square Phrase for dynamic strings with localization [re: https://github.com/square/phrase]
-    // TODO: add hard-coded dimensions to dimens.xml
-    // TODO: update layout files to reference strings from string.xml & dimensions from dimens.xml
-    // TODO: consider maintaining an additional string.xml in Hindi language and / or any of your choice; a right to left language such as Arabic is good for learning mirroring UIs
-    // TODO: add accessibility features to your layouts by adding image content descriptions (re: https://developer.android.com/guide/topics/ui/accessibility/apps.html)
+    // TODO: add accessibility features to your layouts by adding image content descriptions
+    // (re: https://developer.android.com/guide/topics/ui/accessibility/apps.html)
+
+
+
+
+
+    //// TODO: add the source of the BOI.jpeg image to README file
+    //// TODO: add UI screenshots to the README file
+    //// TODO: add hard-coded strings to string.xml;
+    /// consider using xliff:g and / or Square Phrase for dynamic strings with localization
+    /// [re: https://github.com/square/phrase]
+
+    //// TODO: add hard-coded dimensions to dimens.xml
+    //// TODO: consider maintaining an additional string.xml in Hindi language and / or any of your choice;
+    /// a right to left language such as Arabic is good for learning mirroring UIs
+   //// TODO: update layout files to reference strings from string.xml & dimensions from dimens.xml
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
